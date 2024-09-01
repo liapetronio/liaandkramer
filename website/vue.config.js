64e1af89a7380688d4fdc25d67f54c535fd38124
Lia Petronio <51209514+lpetronio@users.noreply.github.com>
2024-08-31 20:49:43 -0400

    add repo to config

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
-};
+    outputDir: 'docs',
+    publicPath: process.env.NODE_ENV === "production" ? "/liaandkramerwedding/" : "/",
+  }
