b0cba4a6578c6e49e84931e4f033b61242e03d94
liapetronio <76866507+liapetronio@users.noreply.github.com>
2025-07-28 14:25:45 -0400

    upload installs and text

---
+++ src/views/HomePage.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <main>
+    <v-container>
+        <v-row>
+            <v-col cols="12" offset-md="2">
+                <h1>Lia and Kramer<br>are to wed</h1>
+      <h3>Saturday August 2, 2025<br>
+    Arsenal Yards<br>Watertown, MA 02472</h3>
+            </v-col>
+        </v-row>
+    </v-container>
+  
+  </main>
+</template>
+
+<style scoped>
+body{
+    font-family: "Inter Tight", sans-serif;
+    font-optical-sizing: auto;
+}
+
+h3{
+    font-size:1.6rem;
+
+}
+h1{
+    font-size: 3.8rem;
+    line-height: 1.2;
+}
+</style>
